83c15b8c06ae923c2114cd832159f0813a48d580
Yu-Hsin Kuo <yuhsink@cs.cmu.edu>
2014-10-17 17:07:23 -0400

    finished DocumentVectorAnnotator

--- src/main/java/edu/cmu/lti/f14/hw3/hw3_yuhsink/annotators/DocumentVectorAnnotator.java
+++ src/main/java/edu/cmu/lti/f14/hw3/hw3_yuhsink/annotators/DocumentVectorAnnotator.java
@@ -6,55 +6,83 @@ import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSList;
 import org.apache.uima.jcas.cas.IntegerArray;
 import org.apache.uima.jcas.cas.StringArray;
 import org.apache.uima.jcas.tcas.Annotation;
 
 import edu.cmu.lti.f14.hw3.hw3_yuhsink.typesystems.Document;
+import edu.cmu.lti.f14.hw3.hw3_yuhsink.typesystems.Token;
+import edu.cmu.lti.f14.hw3.hw3_yuhsink.utils.Utils;
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
-	@Override
-	public void process(JCas jcas) throws AnalysisEngineProcessException {
+  @Override
+  public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
-		if (iter.isValid()) {
-			iter.moveToNext();
-			Document doc = (Document) iter.get();
-			createTermFreqVector(jcas, doc);
-		}
+    FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
+    if (iter.isValid()) {
+      iter.moveToNext();
+      Document doc = (Document) iter.get();
+      createTermFreqVector(jcas, doc);
+    }
 
-	}
+  }
 
-	/**
+  /**
    * A basic white-space tokenizer, it deliberately does not split on punctuation!
-   *
-	 * @param doc input text
-	 * @return    a list of tokens.
-	 */
+   * 
+   * @param doc
+   *          input text
+   * @return a list of tokens.
+   */
 
-	List<String> tokenize0(String doc) {
-	  List<String> res = new ArrayList<String>();
-	  
-	  for (String s: doc.split("\\s+"))
-	    res.add(s);
-	  return res;
-	}
+  List<String> tokenize0(String doc) {
+    List<String> res = new ArrayList<String>();
 
-	/**
-	 * 
-	 * @param jcas
-	 * @param doc
-	 */
+    for (String s : doc.split("\\s+"))
+      res.add(s);
+    return res;
+  }
+  
+  
 
-	private void createTermFreqVector(JCas jcas, Document doc) {
+  /**
+   * 
+   * @param jcas
+   * @param doc
+   */
 
-		String docText = doc.getText();
-		
-		//TO DO: construct a vector of tokens and update the tokenList in CAS
-    //TO DO: use tokenize0 from above 
-		
+  private void createTermFreqVector(JCas jcas, Document doc) {
 
-	}
+    String docText = doc.getText();
+    List <String> tokenizedResult = tokenize0(docText);
+    HashMap <String, Token> table = new HashMap <String, Token>();
+    
+    for (int i = 0; i < tokenizedResult.size(); i++) {
+      String curWord = tokenizedResult.get(i);
+      Token tmp = table.get(curWord);
+      if (tmp == null) {
+        Token tmpToken = new Token(jcas);
+        tmpToken.setText(curWord);
+        tmpToken.setFrequency(1);
+        table.put(curWord, tmpToken);
+      }
+      else {
+        tmp.setFrequency(tmp.getFrequency() + 1);
+        table.put(curWord, tmp);
+      }
+    }
+    
+    ArrayList <Token> tokenArr = new ArrayList <Token> ();
+    for (String word:table.keySet()) {
+      tokenArr.add(table.get(word));
+    }
+    doc.setTokenList(Utils.fromCollectionToFSList(jcas, tokenArr));
+    
+    // TO DO: construct a vector of tokens and update the tokenList in CAS
+    // TO DO: use tokenize0 from above
+
+  }
 
 }
